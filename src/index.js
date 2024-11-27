67a37cbfaa2ef6c9ecab858a9b0a136605b1016a
hs-2171322-Imhanle <2171322@hansung.ac.kr>
2024-11-27 17:40:24 +0900

    port 수정

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const analysisRoutes = require('./routes/analysisRoutes'); //일기상담 라우
 
 
 const app = express();              // Express 애플리케이션 생성
-const PORT = process.env.PORT || 3000; // 포트 설정 (기본값 3000)
+const PORT = process.env.PORT || 443; // 포트 설정 (기본값 3000)
 
 app.use(express.json());
 app.use('/api', diaryRoutes);
