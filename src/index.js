ed9831888908f99dca91f6fa40457f4b57ea64df
hs-2171001-namgungmin <115778281+hs-2171001-namgungmin@users.noreply.github.com>
2024-11-27 18:04:21 +0900

    cors 설정

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ const photoRoutes = require('./routes/photoRoutes'); // 사진 라우트 불러�
 const tagRoutes = require('./routes/tagRoutes');// 태그  라우트 불러오기
 const analysisRoutes = require('./routes/analysisRoutes'); //일기상담 라우트 불러오기
 
-
+const cors = require('cors');
 const app = express();              // Express 애플리케이션 생성
 const PORT = process.env.PORT || 3000; // 포트 설정 (기본값 3000)
+
+// CORS 설정
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization', 'firebase-uid']
+}));
 
 app.use(express.json());
 app.use('/api', diaryRoutes);
