d0be0c6f5b02ec205a917cb26c0420b4de039217
hs-2171322-Imhanle <2171322@hansung.ac.kr>
2024-11-28 17:23:01 +0900

    file path

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -74,7 +74,7 @@ exports.getPhotosByDiaryId = async (req, res) => {
         // file_path를 클라이언트가 접근 가능한 URL로 변환
         const updatedPhotos = photos.map(photo => ({
             ...photo,
-            file_path: `http://<your-server-ip>:3000/uploads/photos/${path.basename(photo.file_path)}`, // 경로 변환
+            file_path: `http://13.124.144.246:3000/uploads/photos/${path.basename(photo.file_path)}`, // 경로 변환
         }));
         
         res.json(updatedPhotos);
